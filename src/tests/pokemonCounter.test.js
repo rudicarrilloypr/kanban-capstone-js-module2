2f84698928007c750a94bd1972c14acb00594ab3
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-07-05 20:27:28 -0600

    Add corrected test

--- src/tests/pokemonCounter.test.js
+++ src/tests/pokemonCounter.test.js
@@ -1,58 +1,50 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { JSDOM } from 'jsdom';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import fetch from 'node-fetch';
+import fetchMock from 'jest-fetch-mock';
 import updatePokemonCounter from '../module/pokemonCounter.js';
 
-global.fetch = fetch;
+// Set up the fetch mock
+global.fetch = fetchMock;
 
 describe('updatePokemonCounter', () => {
   it('correctly counts pokemon items', async () => {
-    const dom = new JSDOM(`
-      <html>
-        <body>
-          <div id="pokemon-counter"></div>
-          <div class="pokemon-item"></div>
-          <div class="pokemon-item"></div>
-          <div class="pokemon-item"></div>
-        </body>
-      </html>
-    `);
+    document.body.innerHTML = `
+      <div id="pokemon-counter"></div>
+      <div class="pokemon-item"></div>
+      <div class="pokemon-item"></div>
+      <div class="pokemon-item"></div>
+    `;
 
-    global.document.body.innerHTML = dom.window.document.body.innerHTML;
+    // Mock a response from the API with three items
+    fetchMock.mockResponseOnce(JSON.stringify({ results: Array(3) }));
+
     await updatePokemonCounter();
     const pokemonCounterElement = document.querySelector('#pokemon-counter');
 
-    expect(pokemonCounterElement.textContent).toBe('6');
+    expect(pokemonCounterElement.textContent).toBe('3');
   });
 
   it('correctly handles no pokemon items', async () => {
-    const dom = new JSDOM(`
-      <html>
-        <body>
-          <div id="pokemon-counter"></div>
-        </body>
-      </html>
-    `);
+    document.body.innerHTML = `
+      <div id="pokemon-counter"></div>
+    `;
 
-    global.document.body.innerHTML = dom.window.document.body.innerHTML;
+    // Mock a response from the API with no items
+    fetchMock.mockResponseOnce(JSON.stringify({ results: [] }));
+
     await updatePokemonCounter();
     const pokemonCounterElement = document.querySelector('#pokemon-counter');
 
-    expect(pokemonCounterElement.textContent).toBe('6');
+    expect(pokemonCounterElement.textContent).toBe('0');
   });
 
-  it('does not throw error if no counter element present', () => {
-    const dom = new JSDOM(`
-      <html>
-        <body>
-          <div class="pokemon-item"></div>
-          <div class="pokemon-item"></div>
-        </body>
-      </html>
-    `);
+  it('does not throw error if no counter element present', async () => {
+    document.body.innerHTML = `
+      <div class="pokemon-item"></div>
+      <div class="pokemon-item"></div>
+    `;
 
-    global.document.body.innerHTML = dom.window.document.body.innerHTML;
+    // Mock a response from the API with two items
+    fetchMock.mockResponseOnce(JSON.stringify({ results: Array(2) }));
 
     expect(() => updatePokemonCounter()).not.toThrow();
   });
